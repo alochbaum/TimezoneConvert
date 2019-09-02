e3e6b1c0062f89cc3f3ae7171864f90397dfc450
Andrew Lochbaum <andrew.lochbaum@live.com>
2019-09-01 23:23:24 -0400

    Copy to clipboard buttons added

--- TimeZoneConvert/MainWindow.xaml.cs
+++ TimeZoneConvert/MainWindow.xaml.cs
@@ -27,11 +27,42 @@ namespace TimeZoneConvert
             // Adding the version number to the title
             MainWin.Title = "Timezone Conert version: " + Assembly.GetExecutingAssembly().GetName().Version;
             dtpInput.Value = DateTime.Now;
-            SetTimes();
+            SetTimes("MM/dd/yy H:mm:ss");
         }
-        private void SetTimes()
+        private void SetTimes(string strFormat)
         {
-            rtbOut1.Text = dtpInput.Value.ToString();
+            DateTime dt = (DateTime)dtpInput.Value;
+            dt = dt.AddHours(-4.0);
+            rtbOut1.Text = dt.ToString(strFormat);
+            dt = dt.AddHours(-1.0);
+            rtbOut2.Text = dt.ToString(strFormat);
+            dt = dt.AddHours(-1.0);
+            rtbOut3.Text = dt.ToString(strFormat);
+            dt = dt.AddHours(-1.0);
+            rtbOut4.Text = dt.ToString(strFormat);
+            dt = dt.AddHours(-1.0);
+            rtbOut5.Text = dt.ToString(strFormat);
+            dt = dt.AddHours(12.0);
+            rtbOut6.Text = dt.ToString(strFormat);
+            dt = dt.AddHours(1.0);
+            rtbOut7.Text = dt.ToString(strFormat);
+            dt = dt.AddHours(1.0);
+            rtbOut8.Text = dt.ToString(strFormat);
+            dt = dt.AddHours(1.0);
+            rtbOut9.Text = dt.ToString(strFormat);
+            dt = dt.AddHours(1.0);
+            rtbOut10.Text = dt.ToString(strFormat);
+
+        }
+
+        private void DtpInput_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            SetTimes("MM/dd/yy H:mm:ss");
+        }
+
+        private void BtnCopy1_Click(object sender, RoutedEventArgs e)
+        {
+
         }
         // note: the code for the original program is located in 
         // \\192.168.3.98\NextBurn\VS2010C#_Projects\TimeZoneConvert
