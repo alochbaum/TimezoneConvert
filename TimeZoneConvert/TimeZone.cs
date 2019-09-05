5621fe4d63dcf00ad5cc6fdd7b70def9585bd0a8
Andrew Lochbaum <andrew.lochbaum@live.com>
2019-09-05 08:31:38 -0400

    finished clase for TixeZone

--- TimeZoneConvert/TimeZone.cs
+++ TimeZoneConvert/TimeZone.cs
@@ -8,8 +8,24 @@ namespace TimeZoneConvert
 {
     class TimeZone
     {
-        public string Title { get; set; }
-        public int ValueX10 { get; set; }
-
+        private int iGroup;
+        private int iId;
+        private string Title;
+        private int ValueX10;
+        public TimeZone(int iGroup, int iId, string Title, int ValueX10)
+        {
+            this.iGroup = iGroup;
+            this.iId = iId;
+            this.Title = Title;
+            this.ValueX10 = ValueX10;
+        }
+        public int GetiGroup() { return iGroup; }
+        public void SetiGroup(int iGroup) { this.iGroup = iGroup; }
+        public int GetiID() { return iId; }
+        public void SetiID(int iID) { this.iId = iId; }
+        public string GetTitle() { return Title; }
+        public void SetTitle(string Title) { this.Title = Title; }
+        public int GetValueX10() { return ValueX10; }
+        public void SetValueX10 (int ValueX10) { this.ValueX10 = ValueX10; }
     }
 }
